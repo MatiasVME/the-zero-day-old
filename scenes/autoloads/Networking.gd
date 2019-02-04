a2ad567b2c04baf9458f358992bf9cf1e778609f
Matías Muñoz Espinoza <writkas2@gmail.com>
2019-02-04 18:52:35 -0300

    Añadiendo nuevas fuentes y empezando a testear el networking

---
+++ scenes/autoloads/Networking.gd
@@ -0,0 +1,37 @@
+extends Node
+
+var port = 1234 # Temporal port
+var max_players = 8 
+
+func _ready():
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+
+func create_host():
+	var host = NetworkedMultiplayerENet.new()
+	host.create_server(port, max_players)
+	get_tree().set_network_peer(host)
+ 
+	_connected_ok()
+
+func join_host(ip):
+	var host = NetworkedMultiplayerENet.new()
+	host.create_client(ip, port)
+	get_tree().set_network_peer(host)
+
+func _connected_ok():
+	rpc("register_player", get_tree().get_network_unique_id())
+	register_player(get_tree().get_network_unique_id())
+#	get_tree().get_root().get_node("Lobby").queue_free()
+	print("_connected_ok")
+	
+remote func register_player(player_id):
+#	var p = ship.instance()
+#	p.set_network_master(player_id)
+#	p.name = str(player_id)
+#	get_tree().get_root().add_child(p)
+	# if I'm the server I inform the new connected player about the others
+	if get_tree().get_network_unique_id() == 1:
+		if player_id != 1:
+			for i in players:
+				rpc_id(player_id, "register_player", i)
+		players.append(player_id)
---
+++ tests/networking/Login.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_Connect_pressed():
+	pass # replace with function body
+
+func _on_CreateHost_pressed():
+	pass # replace with function body
