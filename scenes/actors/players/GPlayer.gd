c990a4a372c235c2857b7569a92266374fda48d7
Matías Muñoz Espinoza <writkas2@gmail.com>
2019-02-05 21:44:29 -0300

    Mejorando el movimiento del personaje

--- scenes/actors/players/GPlayer.gd
+++ scenes/actors/players/GPlayer.gd
@@ -1,14 +1,12 @@
 extends KinematicBody2D
 
-export (int) var speed = 3000
+export (int) var speed = 2000
 
 var move_x
 var move_y
 
 var input_dir : Vector2 = Vector2()
-
-var dir_x = 0
-var dir_y = 0
+var input_run : bool = false
 
 func _ready():
 	pass
@@ -16,38 +14,47 @@ func _ready():
 func _physics_process(delta):
 	input_dir.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	input_dir.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	input_run = Input.is_action_pressed("run")
 	
-	move_x = input_dir.x * speed * delta
-	move_y = input_dir.y * speed * delta
+	if not input_run:
+		move_x = input_dir.x * speed * delta
+		move_y = input_dir.y * speed * delta
+		$Sprite.speed_scale = 0.6
+	else:
+		move_x = input_dir.x * speed * 2 * delta
+		move_y = input_dir.y * speed * 2 * delta
+		$Sprite.speed_scale = 1.2
 	
 	if input_dir.x == -1 and input_dir.y == 0:
-		$Anim.play("RunSide")
+		$Anim.play("MoveSide")
 		$Sprite.flip_h = true
 	elif input_dir.x == -1 and input_dir.y == -1:
-		$Anim.play("RunUp")
+		$Anim.play("MoveUp")
 	elif input_dir.x == 0 and input_dir.y == -1:
-		$Anim.play("RunUp")
+		$Anim.play("MoveUp")
 	elif input_dir.x == 1 and input_dir.y == -1:
-		$Anim.play("RunUp")
+		$Anim.play("MoveUp")
 	elif input_dir.x == 1 and input_dir.y == 0:
-		$Anim.play("RunSide")
+		$Anim.play("MoveSide")
 		$Sprite.flip_h = false
 	elif input_dir.x == 1 and input_dir.y == 1:
-		$Anim.play("RunDown")
+		$Anim.play("MoveDown")
 		$Sprite.flip_h = false
 	elif input_dir.x == 0 and input_dir.y == 1:
-		$Anim.play("RunDown")
+		$Anim.play("MoveDown")
 		$Sprite.flip_h = false
 	elif input_dir.x == -1 and input_dir.y == 1:
-		$Anim.play("RunDown")
+		$Anim.play("MoveDown")
 		$Sprite.flip_h = false
 	else:
 		if $Anim.current_animation != "Idle" or not $Anim.is_playing():
 			$Anim.play("Idle")
+			$Sprite.speed_scale = 0.1
 	
 	if move_x != 0 and move_y != 0:
 		move_x /= 1.5
 		move_y /= 1.5
+		
 	
 	move_and_slide(Vector2(move_x, move_y), Vector2())
 	
