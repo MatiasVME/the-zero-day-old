2905c900a1d25a1afbdb4632fa804b9899b79272
Jjony-dev <developerjjm@gmail.com>
2019-02-11 13:47:20 -0300

    agregando animaciones y movimiento de cañon -no funcional-

--- scenes/actors/vehicles/tanks/Tank.gd
+++ scenes/actors/vehicles/tanks/Tank.gd
@@ -7,7 +7,7 @@ export (float) var MIN_VELOCITY = -400
 export (float) var ACCELERATION = 180
 export (float) var DESACCELERATION = 170
 export (float) var ANGULAR_VELOCITY = 20
-export (float) var CANNON_ANGULAR_VELOCITY = 50
+var min_velocity_to_stop : float = 1 + DESACCELERATION * ( 1.0 / 60.0 ) 
 var dir_move : Vector2
 var dir_rotation : float
 var current_speed : float
@@ -29,11 +29,18 @@ func _move(delta : float) -> void:
 			else:
 				current_speed = MIN_VELOCITY
 	else:
-		if abs(current_speed) > 0.5:
+		if abs(current_speed) > min_velocity_to_stop:
 			current_speed -= DESACCELERATION * sign(current_speed) * delta
 
 func _physics_process(delta):
 	_move(delta)
 	if abs(current_speed) > 10:
-		rotation_degrees += dir_rotation * ANGULAR_VELOCITY * delta
+		rotation_degrees += dir_rotation * (ANGULAR_VELOCITY + abs(current_speed) * 0.01) * delta
+		if $Anim.current_animation != "Foward":
+			$Anim.play("Foward")
+		else:
+			$Anim.playback_speed = sign(current_speed) * 1 + current_speed / MAX_VELOCITY
+	elif $Anim.current_animation != "Idle" or not $Anim.is_playing():
+		$Anim.playback_speed = 1
+		$Anim.play("Idle")
 	move_and_slide(dir_move.rotated(deg2rad(rotation_degrees)) * current_speed * delta, Vector2())
