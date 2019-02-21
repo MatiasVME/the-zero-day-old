2b72be6202c70e291d1ebddc27a3f8db8ff49417
Jean <yanpolperez@gmail.com>
2019-02-21 11:59:02 -0300

    Enemigo Gluton
    Ya con este commit el enemigo Gluton deberia estar completamente funcional, exeptuando por algunos bugs que se solucionaran luego.

--- scenes/actors/enemies/GEnemy.gd
+++ scenes/actors/enemies/GEnemy.gd
@@ -4,73 +4,21 @@ extends KinematicBody2D
 
 signal state_changed
 
-enum STATE{SPAWN, SEEKER, STAND, RUN, ATTACK, DIED}
-
 var state:int = 0
 var current_state:int = 0
-
-var objective:Vector2 = Vector2(1000, 1000) #Favor no cambiar valores
-
-#VARIABLES DE STATE
-export var hp:int
-export var min_hp:int
-export var atk:int
-export var view_distance:float
-export var attack_rangue:float
 
 func _ready():
 	pass
 
+
 func _physics_process(delta):
 	#Estas 4 lineas funcionan como seguro contra tontos.
-	if current_state!=state:
+	if current_state != state:
 		self.emit_signal("state_changed")
 		current_state=state
-	
-	match state:
-		STATE.SPAWN:
-			_spawn()
-		STATE.SEEKER:
-			_seeker()
-		STATE.STAND:
-			_stand()
-		STATE.RUN:
-			_run()
-		STATE.ATTACK:
-			_attack()
-		STATE.DIED:
-			_died()
+
 
 func change_state(state): #Esta funcion es una forma segura de cambiar entre estados.
-	self.state=state
-	self.current_state=state
+	self.state = state
+	self.current_state = state
 	emit_signal("state_changed")
-
-#CADA FUNCION FUNCIONA COMO UN NODO INTERCAMBIANDO EL ESTADO SEGUN LA ENTRADA
-#SOBRESCRIBIR TRAS SER HEREDADA PARA AGREGAR FUNCIONALIDAD
-
-func _spawn():
-	change_state(STATE.STAND)
-
-func _seeker():
-	if get_global_transform().origin.distance_to(objective) > view_distance:
-		change_state(STATE.STAND)
-	elif get_global_transform().origin.distance_to(objective) < attack_rangue:
-		change_state(STATE.ATTACK)
-	elif hp <= min_hp:
-		change_state(STATE.RUN)
-
-func _stand():
-	if get_global_transform().origin.distance_to(objective) < view_distance:
-		change_state(STATE.SEEKER)
-
-func _run():
-	if hp > min_hp:
-		change_state(STATE.STAND)
-
-func _attack():
-	if get_global_transform().origin.distance_to(objective) < attack_rangue:
-		change_state(STATE.SEEKER)
-
-func _died():
-	pass
--- scenes/actors/enemies/gluton/Gluton.gd
+++ scenes/actors/enemies/gluton/Gluton.gd
@@ -2,5 +2,95 @@
 
 extends "res://scenes/actors/enemies/GEnemy.gd"
 
+var hp:int = 10
+var speed:int = 2
+var attack:int = 2
+
+var view_distance:float = 10
+var attack_distance:float = 1
+
+var objective:Vector2
+
 func _ready():
-	pass
+	self.state = 2
+
+func _physics_process(delta):
+	
+	match state:
+		0: #STAND
+			$Body.play("Idle")
+		1: #SEEKER
+			pass
+		2: #RUN
+			objective = get_global_mouse_position()
+			play_animation(objective)
+			move_and_slide(get_direction(get_global_mouse_position()).normalized()*5)
+		3: #ATTACK
+			pass
+
+
+func get_direction(objective):
+	if transform.origin.angle_to_point(objective) < 2.5 and transform.origin.angle_to_point(objective) > 0.5:
+		return Vector2(0, 1)
+	elif transform.origin.angle_to_point(objective) > -2.5 and transform.origin.angle_to_point(objective) < -0.5:
+		return Vector2(0, -1)
+	elif transform.origin.angle_to_point(objective) < 0.5 and transform.origin.angle_to_point(objective) > -0.5:
+		return Vector2(1, 0)
+	elif transform.origin.angle_to_point(objective) > 2.5 and transform.origin.angle_to_point(objective) < -2:
+		return Vector2(-1, 0)
+	else:
+		return Vector2(0, 0)
+
+func get_direction_to_see(objective): #Devuelve la direccion en grados
+	if round(transform.origin.angle_to_point(objective)) == 2:
+		return 0
+	elif round(transform.origin.angle_to_point(objective)) == -2:
+		return 180
+	elif round(transform.origin.angle_to_point(objective)) == 0:
+		return -90
+	elif round(transform.origin.angle_to_point(objective)) == 3:
+		return 90
+	else:
+		return $Body.get_animation()
+
+func get_direction_to_see_inverse(objective): #Devuelve la direccion en grados
+	if round(transform.origin.angle_to_point(objective)) == 2:
+		return 180
+	elif round(transform.origin.angle_to_point(objective)) == -2:
+		return 0
+	elif round(transform.origin.angle_to_point(objective)) == 0:
+		return 90
+	elif round(transform.origin.angle_to_point(objective)) == 3:
+		return -90
+	else:
+		return $Body.get_animation()
+
+func play_animation(objective):
+	if state == 2:
+		match get_direction_to_see_inverse(objective):
+			0:
+				$Body.play("Run_Up")
+			180:
+				$Body.play("Run_Down")
+			90:
+				if $Body.flip_h:
+					$Body.flip_h = false
+				$Body.play("Run_Side")
+			-90:
+				if !$Body.flip_h:
+					$Body.flip_h = true
+				$Body.play("Run_Side")
+	else:
+		match get_direction_to_see(objective):
+			0:
+				$Body.play("Run_Up")
+			180:
+				$Body.play("Run_Down")
+			90:
+				if $Body.flip_h:
+					$Body.flip_h = false
+				$Body.play("Run_Side")
+			-90:
+				if !$Body.flip_h:
+					$Body.flip_h = true
+				$Body.play("Run_Side")
