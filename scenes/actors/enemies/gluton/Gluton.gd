7a844f18c9229c80451b905ca2b69f0b7edbd6c3
Jean <yanpolperez@gmail.com>
2019-02-28 13:05:39 -0300

    Gluton Interactua con GPlayer
    El enemigo Gluton ya puede ejecutar la mayoria de sus funciones, exceptuando las de combate.

--- scenes/actors/enemies/gluton/Gluton.gd
+++ scenes/actors/enemies/gluton/Gluton.gd
@@ -9,88 +9,102 @@ var attack:int = 2
 var view_distance:float = 10
 var attack_distance:float = 1
 
-var objective:Vector2
+onready var objective = 0
 
 func _ready():
-	self.state = 2
+	self.state = 0
+
+func _process(delta):
+	pass
 
 func _physics_process(delta):
-	
-	match state:
-		0: #STAND
-			$Body.play("Idle")
-		1: #SEEKER
-			pass
-		2: #RUN
-			objective = get_global_mouse_position()
-			play_animation(objective)
-			move_and_slide(get_direction(get_global_mouse_position()).normalized()*5)
-		3: #ATTACK
-			pass
-
-
-func get_direction(objective):
-	if transform.origin.angle_to_point(objective) < 2.5 and transform.origin.angle_to_point(objective) > 0.5:
-		return Vector2(0, 1)
-	elif transform.origin.angle_to_point(objective) > -2.5 and transform.origin.angle_to_point(objective) < -0.5:
-		return Vector2(0, -1)
-	elif transform.origin.angle_to_point(objective) < 0.5 and transform.origin.angle_to_point(objective) > -0.5:
-		return Vector2(1, 0)
-	elif transform.origin.angle_to_point(objective) > 2.5 and transform.origin.angle_to_point(objective) < -2:
-		return Vector2(-1, 0)
-	else:
-		return Vector2(0, 0)
+	if objective:
+		match state:
+			0: #STAND
+				$Body.play("Idle")
+				if self.get_global_position().distance_to(objective.get_global_position()) < 100:
+					change_state(1)
+				if self.hp < 3:
+					change_state(2)
+			1: #SEEKER
+				sekeer(objective)
+				if self.get_global_position().distance_to(objective.get_global_position()) > 100 and objective as GPlayer:
+					change_state(0)
+					"""
+				if self.get_global_position().distance_to(objective) < 20:
+					change_state(3)
+					"""
+				if self.hp < 3:
+					change_state(2)
+			2: #RUN
+				run(objective)
+				if self.hp > 3:
+					change_state(0)
+			3: #ATTACK
+				objective.damage(10)
+				if self.get_global_position().distance_to(objective.get_global_position()) > 20:
+					change_state(1)
+				if self.hp < 3:
+					change_state(2)
+			4: #DIE
+				self.queue_free()
 
 func get_direction_to_see(objective): #Devuelve la direccion en grados
-	if round(transform.origin.angle_to_point(objective)) == 2:
+	if round(transform.origin.angle_to_point(objective.get_global_position())) == 2:
 		return 0
-	elif round(transform.origin.angle_to_point(objective)) == -2:
+	elif round(transform.origin.angle_to_point(objective.get_global_position())) == -2:
 		return 180
-	elif round(transform.origin.angle_to_point(objective)) == 0:
+	elif round(transform.origin.angle_to_point(objective.get_global_position())) == 0:
 		return -90
-	elif round(transform.origin.angle_to_point(objective)) == 3:
+	elif round(transform.origin.angle_to_point(objective.get_global_position())) == 3:
 		return 90
 	else:
 		return $Body.get_animation()
 
-func get_direction_to_see_inverse(objective): #Devuelve la direccion en grados
-	if round(transform.origin.angle_to_point(objective)) == 2:
-		return 180
-	elif round(transform.origin.angle_to_point(objective)) == -2:
-		return 0
-	elif round(transform.origin.angle_to_point(objective)) == 0:
-		return 90
-	elif round(transform.origin.angle_to_point(objective)) == 3:
-		return -90
-	else:
-		return $Body.get_animation()
+func sekeer(objective): #Rutina en caso de que vea al objetivo
+	match get_direction_to_see(objective):
+		0:
+			$Body.play("Run_Up")
+			self.move_and_slide(Vector2(0, -10))
+		180:
+			$Body.play("Run_Down")
+			self.move_and_slide(Vector2(0, 10))
+		90:
+			if $Body.flip_h:
+				$Body.flip_h = false
+			$Body.play("Run_Side")
+			self.move_and_slide(Vector2(10, 0))
+		-90:
+			if !$Body.flip_h:
+				$Body.flip_h = true
+			$Body.play("Run_Side")
+			self.move_and_slide(Vector2(-10, 0))
 
-func play_animation(objective):
-	if state == 2:
-		match get_direction_to_see_inverse(objective):
-			0:
-				$Body.play("Run_Up")
-			180:
-				$Body.play("Run_Down")
-			90:
-				if $Body.flip_h:
-					$Body.flip_h = false
-				$Body.play("Run_Side")
-			-90:
-				if !$Body.flip_h:
-					$Body.flip_h = true
-				$Body.play("Run_Side")
-	else:
-		match get_direction_to_see(objective):
-			0:
-				$Body.play("Run_Up")
-			180:
-				$Body.play("Run_Down")
-			90:
-				if $Body.flip_h:
-					$Body.flip_h = false
-				$Body.play("Run_Side")
-			-90:
-				if !$Body.flip_h:
-					$Body.flip_h = true
-				$Body.play("Run_Side")
+func run(objective): #Rutina en caso de tener que huir del objetivo
+	match get_direction_to_see(objective):
+		0:
+			$Body.play("Run_Down")
+			self.move_and_slide(Vector2(0, 10))
+			
+		180:
+			$Body.play("Run_Up")
+			self.move_and_slide(Vector2(0, -10))
+		90:
+			if !$Body.flip_h:
+				$Body.flip_h = true
+			$Body.play("Run_Side")
+			self.move_and_slide(Vector2(-10, 0))
+		-90:
+			if $Body.flip_h:
+				$Body.flip_h = false
+			$Body.play("Run_Side")
+			self.move_and_slide(Vector2(10, 0))
+
+func _on_Area2D_body_entered(body):
+	if body as GPlayer:
+		objective = body
+
+
+func _on_Area2D_body_exited(body):
+	if body as GPlayer:
+		objective = 0
