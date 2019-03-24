61f55a6ae03d49d9a40b512ad4aa94da8ca748c6
Matías Muñoz Espinoza <writkas2@gmail.com>
2019-03-24 01:15:18 -0300

    Balas chocan con las murallas y el agua -_-'

--- scenes/actors/bullets/GBullet.gd
+++ scenes/actors/bullets/GBullet.gd
@@ -14,11 +14,14 @@ var direction : Vector2
 # hace, por ejemplo. Una torreta dispara y le asigna
 # 2 de daño a la bala y la bala cuando choca con el
 # player le hace 2 de daño.
-var damage = 1
+var damage := 1
 
 # Previene que se haga la animacion de dead mas
 # de una vez.
-var is_mark_to_dead = false
+var is_mark_to_dead := false
+
+var collision
+var collision_count := 0
 
 func _ready():
 	$Sprite.playing = true
@@ -26,7 +29,16 @@ func _ready():
 func _physics_process(delta):
 	if trajectory == Trajectory.LINEAL:
 		move_and_slide(direction * 250)
-
+	
+	collision_count = get_slide_count()
+	
+	if not is_mark_to_dead and collision_count > 0:
+		for i in collision_count:
+			collision = get_slide_collision(i)
+			print(collision)
+			if collision.collider.is_in_group("Terrain") or collision.collider.is_in_group("Enviroment"):
+				dead()
+	
 func dead():
 	if not is_mark_to_dead:
 		is_mark_to_dead = true
