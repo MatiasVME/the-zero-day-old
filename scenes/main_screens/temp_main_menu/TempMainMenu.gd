ef9731cdb198fb248115af3251557e8682cc52ba
Matías Muñoz Espinoza <writkas2@gmail.com>
2019-04-14 12:36:21 -0400

    Cambiando la cámara en el menu y el mapa1

--- scenes/main_screens/temp_main_menu/TempMainMenu.gd
+++ scenes/main_screens/temp_main_menu/TempMainMenu.gd
@@ -1,21 +1,20 @@
 extends Node2D
 
 func _ready():
-	CameraManager.set_camera_menu()
-	CameraManager.current_camera.smoothing_speed = 15
-	CameraManager.current_camera.global_position = global_position
+	$Camera.smoothing_speed = 15
+	$Camera.global_position = global_position
 	
 	$Version.text = Main.VERSION
 
 func _on_Back_pressed():
-	CameraManager.current_camera.global_position = global_position
+	$Camera.global_position = global_position
 
 func _on_ButtonCredits_pressed():
-	CameraManager.current_camera.global_position = $Credits.global_position
+	$Camera.global_position = $Credits.global_position
 	$Credits._resume()
 
 func _on_BackFromCredits_pressed():
-	CameraManager.current_camera.global_position = global_position
+	$Camera.global_position = global_position
 
 func _on_ButtonAdventureMode_pressed():
-	CameraManager.current_camera.global_position = $AdventureMode.global_position
+	$Camera.global_position = $AdventureMode.global_position
--- scenes/maps/adventure_mode/main_history/chapter_1/Map1Full.gd
+++ scenes/maps/adventure_mode/main_history/chapter_1/Map1Full.gd
@@ -8,9 +8,8 @@ func _ready():
 	add_child(player)
 	player.enable_player()
 	
-	camera = CameraManager.set_camera_game()
-	camera.following = player
-	camera.mode = camera.Mode.FOLLOW
+	$GameCamera.following = player
+	$GameCamera.mode = $GameCamera.Mode.FOLLOW
 	
 	$HUD.set_hud_actor(player)
 	
