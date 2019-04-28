7589e02c0dce442a71a90a76016220ed1b8f0a4b
Matías Muñoz Espinoza <writkas2@gmail.com>
2019-04-27 21:46:32 -0400

    El resto de los bots al construir el core

--- scenes/structures/core/Core.gd
+++ scenes/structures/core/Core.gd
@@ -1,6 +1,7 @@
 extends GStructure
 
 onready var game_camera = get_tree().get_nodes_in_group("GameCamera")
+onready var hud = get_tree().get_nodes_in_group("HUD")
 
 func _init():
 	structure_size = StructureSize.S3X3
@@ -15,12 +16,24 @@ func _ready():
 		print("Falta la GameCamera")
 		game_camera = null
 	
+	if hud.size() > 0:
+		hud = hud[0]
+	else:
+		print("Falta el HUD")
+		hud = null
+	
 func init_player(player_name):
 	var player
 	
 	match player_name:
 		"Dorbot":
 			player = PlayerManager.init_player(0)
+		"Matbot":
+			player = PlayerManager.init_player(1)
+		"Pixbot":
+			player = PlayerManager.init_player(2)
+		"Serbot":
+			player = PlayerManager.init_player(3)
 			
 	get_parent().add_child(player)
 	player.position = position + Vector2(0, 30)
@@ -29,3 +42,5 @@ func init_player(player_name):
 	if game_camera:
 		game_camera.following = player
 		game_camera.mode = game_camera.Mode.FOLLOW
+		
+	if hud: hud.set_hud_actor(player)
