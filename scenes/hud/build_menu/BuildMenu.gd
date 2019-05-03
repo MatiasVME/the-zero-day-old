e0e283b06ad970d9b2414a8da8105e4b2517c15f
Matías Muñoz Espinoza <writkas2@gmail.com>
2019-05-03 12:34:54 -0400

    Cambiando el BuildPanel

--- scenes/hud/build_menu/BuildMenu.gd
+++ scenes/hud/build_menu/BuildMenu.gd
@@ -3,7 +3,8 @@ extends Node2D
 signal build_pressed()
 
 func _ready():
-	$Background/Scroll/Grid/LightTurret.build_id = Enums.StructureType.LIGHT_TURRET
-
+	$Background/Grid/LightTurret.build_id = Enums.StructureType.LIGHT_TURRET
+	
 func _on_LightTurret_pressed():
 	BuildManager.prepare_to_build($Background/Scroll/Grid/LightTurret.build_id)
+	
