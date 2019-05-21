52f5b24d6e9e97ca7ccf06fc13b3ed9913bb61ac
Matías Muñoz Espinoza <writkas2@gmail.com>
2019-05-21 16:06:32 -0400

    Cambiando los botones del menu principal

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -9,16 +9,16 @@ func _ready():
 func _on_Back_pressed():
 	$Camera.global_position = global_position
 
-func _on_ButtonCredits_pressed():
-	$Camera.global_position = $Credits.global_position
-	$Credits._resume()
-
 func _on_BackFromCredits_pressed():
 	$Camera.global_position = global_position
-
-func _on_ButtonAdventureMode_pressed():
-	$Camera.global_position = $AdventureMode.global_position
 
 func _on_DeleteData_pressed():
 	DataManager.remove_all_data()
 	get_tree().quit()
+
+func _on_Credits_pressed():
+	$Camera.global_position = $Credits.global_position
+	$Credits._resume()
+
+func _on_Play_pressed():
+	$Camera.global_position = $AdventureMode.global_position
