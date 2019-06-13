0ab0cdf3f86ccc0dd35f5e8cf37a03278bec5a67
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2019-06-13 15:27:35 -0400

    Botones para seleccionar items en la hotbar

--- scenes/controls/Mobile/Buttons/Buttons.gd
+++ scenes/controls/Mobile/Buttons/Buttons.gd
@@ -2,9 +2,17 @@ extends Node2D
 
 signal fire
 signal select
+signal select_next_item_up
+signal select_next_item_down
 
 func _on_Fire_pressed():
 	emit_signal("fire")
 
 func _on_Select_pressed():
 	emit_signal("select")
+
+func _on_SelectNextItemUp_pressed():
+	emit_signal("select_next_item_up")
+
+func _on_SelectNextItemDown_pressed():
+	emit_signal("select_next_item_down")
