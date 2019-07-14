e3727aa6bde883cdaf84ec92a46ad939f83e38e5
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2019-07-14 15:12:38 -0400

    WIP: PlayerMenu-PlayersInfo

--- scenes/hud/player_menu/PlayerMenu.gd
+++ scenes/hud/player_menu/PlayerMenu.gd
@@ -5,29 +5,34 @@ func _on_Config_toggled(button_pressed):
 		$Panels/Menu.show()
 		$Panels/Inventory.hide()
 		$Panels/Skills.hide()
+		$Panels/PlayersInfo.hide()
 		
+		$InfoPlayers.pressed = false
 		$Inventory.pressed = false
 		$Skills.pressed = false
 	else:
 		$Panels/Menu.hide()
 		$Panels/Inventory.hide()
 		$Panels/Skills.hide()
+		$Panels/PlayersInfo.hide()
 		
 		$Anim.play_backwards("show")
-		get_parent().get_node("Index/TogglePlayerMenu")
 
 func _on_Inventory_toggled(button_pressed):
 	if button_pressed:
 		$Panels/Menu.hide()
 		$Panels/Inventory.show()
 		$Panels/Skills.hide()
+		$Panels/PlayersInfo.hide()
 		
+		$InfoPlayers.pressed = false
 		$Config.pressed = false
 		$Skills.pressed = false
 	else:
 		$Panels/Menu.hide()
 		$Panels/Inventory.hide()
 		$Panels/Skills.hide()
+		$Panels/PlayersInfo.hide()
 		
 		$Anim.play_backwards("show")
 
@@ -36,12 +41,33 @@ func _on_Skills_toggled(button_pressed):
 		$Panels/Menu.hide()
 		$Panels/Inventory.hide()
 		$Panels/Skills.show()
+		$Panels/PlayersInfo.hide()
 		
+		$InfoPlayers.pressed = false
 		$Config.pressed = false
 		$Inventory.pressed = false
 	else:
 		$Panels/Menu.hide()
 		$Panels/Inventory.hide()
 		$Panels/Skills.hide()
+		$Panels/PlayersInfo.hide()
+		
+		$Anim.play_backwards("show")
+
+func _on_InfoPlayers_toggled(button_pressed):
+	if button_pressed:
+		$Panels/Menu.hide()
+		$Panels/Inventory.hide()
+		$Panels/Skills.hide()
+		$Panels/PlayersInfo.show()
+		
+		$Skills.pressed = false
+		$Config.pressed = false
+		$Inventory.pressed = false
+	else:
+		$Panels/Menu.hide()
+		$Panels/Inventory.hide()
+		$Panels/Skills.hide()
+		$Panels/PlayersInfo.hide()
 		
 		$Anim.play_backwards("show")
