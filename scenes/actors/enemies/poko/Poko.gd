a8b35ffd46a3865fc9f10a6bdced694fb9b4eae6
Nahum Santana <nahumsantanam@gmail.com>
2019-07-22 16:11:52 -0400

    WIP: Poko, Comportamiento Basico Parte 2
    Poko ahora es capaz de seguir al jugador.
    Cuando Poko ve al jugador dentro de su rango lo persigue, durante un tiempo y luego se cansa.

--- scenes/actors/enemies/poko/Poko.gd
+++ scenes/actors/enemies/poko/Poko.gd
@@ -3,14 +3,13 @@ extends "res://scenes/actors/enemies/GEnemy.gd"
 onready var max_speed = 50
 onready var speed = Vector2(0,0)
 onready var acceleration = 50
-onready var move_dir = 0
+onready var move_dir = rand_range(0,360)
 onready var is_waiting = false
+onready var objective = null
 
 func _ready():
 	state = State.RANDOM_WALK
 	$TimerWalk.start()
-	
-	randomize()
 	
 func _physics_process(delta):
 	if speed.x > 0: $Sprite.flip_h = true
@@ -18,9 +17,16 @@ func _physics_process(delta):
 	
 	match state:
 		State.SEEKER:
-			speed.x = cos(deg2rad(move_dir))
-			speed.y = sin(deg2rad(move_dir))
-			move_and_slide(speed*acceleration)
+			if is_waiting == false:
+				if objective != null:
+					var delta_x = global_position.x-objective.global_position.x
+					var delta_y = global_position.y-objective.global_position.y
+					
+					speed.x = -delta_x/acceleration*2
+					speed.y = -delta_y/acceleration*2
+					
+			
+				move_and_slide(speed*acceleration)
 			
 		State.RANDOM_WALK:
 			if is_waiting == false:
@@ -42,6 +48,10 @@ func _on_TimerWait_timeout():
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
 		state = State.SEEKER
-		print((move_dir))
-		move_dir = global_position.angle_to(body.global_position)
+		objective = body
 	
+
+func _on_DetectArea_body_exited(body):
+	if body.is_in_group("Player"):
+		state = State.RANDOM_WALK
+		objective = null
