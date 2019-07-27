defc48bac9a6b6904c6e64c1c7006a79d4ef226a
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2019-07-27 15:37:37 -0400

    WIP: Test Navigation

--- scenes/tiles/tilemaps/GTilemap.gd
+++ scenes/tiles/tilemaps/GTilemap.gd
@@ -1,7 +1,33 @@
 extends Navigation2D
 
+# Mapa el cual contendrá todas las navegaciones,
+# y colisiones
 var nav_map
+# El resto de mapas
+var other_maps
 
 func _ready():
+	# Obtenemos el mapa principal, en este caso
+	# terrain
 	nav_map = get_children()[0]
-	print(nav_map.name)
+	other_maps = get_other_maps(nav_map)
+	
+	add_not_navigable_to_current_nav_map()
+	
+# map_exception no se añadirá a la lista
+func get_other_maps(map_exception):
+	var other_maps := []
+	
+	for posible_map in get_children():
+		if posible_map is TileMap and posible_map != map_exception:
+			other_maps.append(posible_map)
+			
+	return other_maps
+
+func add_not_navigable_to_current_nav_map():
+	for other_map in other_maps:
+		var x := 0
+		var y := 0
+		
+		while y <= other_map.tile_set.get_used_rect().size - 1:
+			pass
