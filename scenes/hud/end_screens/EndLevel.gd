83baa03c014cd16035c687d82acefbd738accdc2
Matías <writkas2@gmail.com>
2019-10-21 16:21:30 -0300

    Se muestra el dinero ganado al morir

--- scenes/hud/end_screens/EndLevel.gd
+++ scenes/hud/end_screens/EndLevel.gd
@@ -10,10 +10,6 @@ func _ready():
 		lose()
 	else:
 		print_debug("No se a ganado ni perdido")
-		pass
-		
-	Main.connect("win_adventure", self, "_on_win_adventure")
-	Main.connect("lose_adventure", self, "_on_lose_adventure")
 	
 	$MoneyDisplay.amount_to_show(Main.store_money)
 	
@@ -23,21 +19,12 @@ func win():
 func lose():
 	$Title.text = "YOU LOSE.."
 	
-	Main.store_money = int(round(Main.store_money / 2))
-	
-	print_debug("Lose, ", Main.store_money)
+	Main.store_money = round(Main.store_money / 2)
 
-func _on_win_adventure():
-	win()
-	
-func _on_lose_adventure():
-	lose()
-	
 func _on_Next_pressed():
 	pass
 
 func _on_Menu_pressed():
-#	Main.prepare_to_exit()
 	get_tree().change_scene("res://scenes/Main.tscn")
 
 func _on_Restart_pressed():
