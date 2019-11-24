200b81bd17906c1880e9b3c6515720e4b0e49eae
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2019-11-24 11:55:44 -0300

    Añadiendo a LEVC en los créditos

--- scenes/maps/adventure_mode/main_history/end/TheEnd.gd
+++ scenes/maps/adventure_mode/main_history/end/TheEnd.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready():
-	MusicManager.play(MusicManager.Music.SOLITUDE)
+	MusicManager.play(MusicManager.Music.SHOP_THEME)
 	$FullCredits/Anim.play("Show")
 
 func _on_Menu_pressed():
