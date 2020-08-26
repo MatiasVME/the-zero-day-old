5f9155df8be46ca66dda4a33127f33e23d694355
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2020-08-26 14:00:25 -0400

    Se añade script de GEnemy

--- scenes/actors/enemies/GEnemy.gd
+++ scenes/actors/enemies/GEnemy.gd
@@ -1,3 +1,33 @@
 extends KinematicBody2D
 
 class_name GEnemy
+
+var damage_indicator_label = preload("res://scenes/hud/floating_hud/FloatingText.tscn")
+
+# Debe tener esta variable para que la camara pueda seguir a este
+# enemigo. Ya que los GPlayer tienen esta variable.
+# NEEDFIX
+var selected_num := -1 # -1 es nignuno seleccionado
+
+var was_attacked_by_a_player := false
+
+enum State {
+	STAND,
+	SEEKER,
+	RUN,
+	ATTACK,
+	DIE,
+	RANDOM_WALK,
+	STUNNED
+}
+var state := 0
+var current_state := 0
+
+# Puede recibir daño ?
+var can_damage = true
+
+# Contine el TZDEnemy con todos los datos
+# y eventos que invulucra
+var data : TZDEnemy
+
+signal state_changed
